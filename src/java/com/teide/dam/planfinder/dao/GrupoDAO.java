f907058cb4817a35272c734624dc0f05f997ef4c
Mario <Mario@192.168.1.11>
2014-02-02 19:52:17 +0100

    Alta y busqueda de grupoDAO hecho, en eliminar y modificar hay dudas

--- src/java/com/teide/dam/planfinder/dao/GrupoDAO.java
+++ src/java/com/teide/dam/planfinder/dao/GrupoDAO.java
@@ -4,7 +4,13 @@
  */
 package com.teide.dam.planfinder.dao;
 
+import com.teide.dam.planfinder.bbdd.Queries;
+import com.teide.dam.planfinder.pojos.Grupo;
+import com.teide.dam.planfinder.pojos.Tipo;
+import com.teide.dam.planfinder.util.HibernateUtil;
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -13,12 +19,41 @@ import org.hibernate.Session;
 public class GrupoDAO extends GenericDAO{
     
     /*
-     
-     
-     
+     Grupo--> Añadir/Crear, modificar, borrar/salir,buscar...
+     El crear es altagrupo, las dos busquedas por nombre y tipo, en borrar hay que cambiar el estado=false
+     Preguntar por el modificar
      */
     public GrupoDAO(Session session) {
         super(session);
+    }
+    
+    public static void altaGrupo (Grupo g){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(g);
+        tx.commit();
+    }
+    
+    public Grupo buscarGrupoNombre (String nombre){
+        Query q = getSession().createQuery(Queries.BUSCAR_GRUPO_NOMBRE);
+        q.setParameter("nombre", nombre);
+        return (Grupo)q.uniqueResult();
+    }
+    
+    public Grupo buscarGrupoTipo (Tipo tipo){
+        Query q = getSession().createQuery(Queries.BUSCAR_GRUPO_TIPO);
+        q.setParameter("tipo", tipo);
+        return (Grupo)q.uniqueResult();
+    }
+    
+    public void eliminarGrupo (String nombre){
+        Query q = getSession().createQuery(Queries.BUSCAR_GRUPO_NOMBRE);
+        q.setParameter("nombre", nombre);
+        Grupo.setEstado=false;
+    }
+    
+    public void modificarGrupo(){
+    
     }
     
 }
