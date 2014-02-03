141791f73afc4949eb243309f6089ee05c7efafe
Jose <Jose@Jose-PC>
2014-02-03 18:37:49 +0100

    Empezamos copieteando lo de Miguel X)

--- src/java/com/teide/dam/planfinder/bbdd/Queries.java
+++ src/java/com/teide/dam/planfinder/bbdd/Queries.java
@@ -9,7 +9,9 @@ package com.teide.dam.planfinder.bbdd;
  * @author dam2
  */
 public class Queries {
+    //Queries de grupo
     public static final String BUSCAR_GRUPO_NOMBRE = "from Grupo as g where g.nombre=:nombre";
     public static final String BUSCAR_GRUPO_TIPO = "from Grupo as g where g.tipo=:tipo";
-    
+    //Queries de tipo
+    public static final String BUSCAR_TIPO_NOMBRE = "from Tipo as t where t.IdTipo=:IdTipo";
 }
