841120a5d494c5c72b3fc3cec2d975a93f096a68
dam2 <dam2@dam2-System-Product-Name>
2014-02-04 09:55:01 +0100

    FUNCIONES - comprobarUsuario - insertarUsuario - editarUsuario - actualizarPosicionUsuario
    QUERIES
    - BUSCAR_USUARIO_SIM

--- src/java/com/teide/dam/planfinder/bbdd/Queries.java
+++ src/java/com/teide/dam/planfinder/bbdd/Queries.java
@@ -14,5 +14,6 @@ public class Queries {
     public static final String BUSCAR_GRUPO_TIPO = "from Grupo as g where g.tipo=:tipo";
     //Queries de tipo
     public static final String BUSCAR_TIPO_NOMBRE = "from Tipo as t where t.IdTipo=:IdTipo";
-    
+    //Queries de Usuario
+    public static final String BUSCAR_USUARIO_SIM = "from Usuario as u where u.Sim=:Sim";
 }
--- src/java/com/teide/dam/planfinder/dao/UsuarioDAO.java
+++ src/java/com/teide/dam/planfinder/dao/UsuarioDAO.java
@@ -4,6 +4,11 @@
  */
 package com.teide.dam.planfinder.dao;
 
+import com.teide.dam.planfinder.bbdd.Queries;
+import com.teide.dam.planfinder.pojos.Usuario;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
@@ -21,6 +26,44 @@ public class UsuarioDAO extends GenericDAO{
         super(session);
     }
     
+    public String insertarUsuario(String sim, String nombre, int radioRecepcion, double latitud, double longitud, Date ultimaConexion){
+   
+        GregorianCalendar gc = new GregorianCalendar();
+        ultimaConexion = gc.getTime();
+        Usuario u = new Usuario(sim, nombre, "disponible", radioRecepcion, latitud, longitud, ultimaConexion);
+        try {
+            getSession().persist(u);
+            return "OK";
+        } catch (Exception e) {
+            return "NOK";
+        }        
+    }
     
+    public Usuario comprobarUsuario(String sim){
+        Query q = getSession().createQuery(Queries.BUSCAR_USUARIO_SIM);
+        q.setParameter("sim", q);
+        return (Usuario)q.uniqueResult();
+    }
     
+    public String editarUsuario(String sim, String nombre, String estado, int radioRecepcion){
+        Usuario u = comprobarUsuario(sim);
+        if (u!=null) {
+            u.setEstado(estado);
+            u.setNombre(nombre);
+            u.setRadioRecepcion(radioRecepcion);
+            return "OK";
+        }
+        else return "NOK";        
+    }
+    
+    public String actualizarPosicionUsuario(String sim, double latitud, double longitud){
+        Usuario u = comprobarUsuario(sim);
+        if (u!=null){
+            u.setLatitud(latitud);
+            u.setLongitud(longitud);
+            return "OK";
+        }
+        else return "NOK";
+    }  
+      
 }
