447df74cfb8e89e4e74abd26f52d0514a45d2183
dam2 <dam2@dam2-System-Product-Name>
2014-02-18 10:13:15 +0100

    Modifico Estados añadiendo mas, cambio UsuarioDao,UbicacionDAO e InsertarUsuario, con los errores que vimos el otro dia

--- src/java/com/teide/dam/planfinder/util/Estados.java
+++ src/java/com/teide/dam/planfinder/util/Estados.java
@@ -15,4 +15,13 @@ public interface Estados {
     public static final String SOLICITADO = "solicitado";
     public static final String BANEADO = "baneado";
     public static final String NOSOLICITADO = "nosolicitado";
+    
+    //usuarios
+    public static final String INVISIBLE = "invisible";
+    public static final String VISIBLE = "visible";
+    
+    //estados grupos
+    public static final String HABILITADO = "habilitado";
+    public static final String NOHABILITADO = "nohabilitado";
+    
 }
