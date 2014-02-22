3b5aca1d688967d9f64db36025b9a78801a8f515
Mario <Mario@192.168.1.11>
2014-02-22 11:19:13 +0100

    hecho mi servlet de buscarNombreGrupo

---
+++ src/java/com/teide/dam/planfinder/servlets/BuscarNombeGrupoServlet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teide.dam.planfinder.servlets;
+
+import com.teide.dam.planfinder.dao.GrupoDAO;
+import com.teide.dam.planfinder.pojos.Grupo;
+import com.teide.dam.planfinder.util.HibernateUtil;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Mario
+ */
+public class BuscarNombeGrupoServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String nombre = req.getParameter("nombre");
+        if (nombre == null || nombre.trim().isEmpty()){
+            out.println("NOK");
+        }
+        else{
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            Transaction tx = session.beginTransaction();
+            GrupoDAO gDAO = new GrupoDAO(session);
+            ArrayList<Grupo> g = gDAO.buscarGrupoNombre(nombre);
+            Gson json = new Gson();
+            String resultado = json.toJson(g);
+            out.println(resultado);
+        }
+    }
+
+}
