9f341a7a0e48d829d9b1c00cc6d7e735a549a6b8
dam2 <dam2@dam2-System-Product-Name>
2014-02-25 10:00:32 +0100

    Prueba AntonioDAO

---
+++ src/java/com/teide/dam/planfinder/dao/AntonioDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teide.dam.planfinder.dao;
+
+/**
+ *
+ * @author dam2
+ */
+public class AntonioDAO {
+    
+}
