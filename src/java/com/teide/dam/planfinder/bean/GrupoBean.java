1f3e97ed992d3830e896d84edd31939e810afc73
dam2 <dam2@dam2-System-Product-Name>
2014-02-27 13:16:18 +0100

    COMPROBARUSUARIOSERVLET devuelve lo que Guille quiere

---
+++ src/java/com/teide/dam/planfinder/bean/GrupoBean.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teide.dam.planfinder.bean;
+
+/**
+ *
+ * @author dam2
+ */
+public class GrupoBean {
+    
+    private int idGrupo;
+    private String nombreGrupo, descripcion;
+
+    public GrupoBean(int idGrupo, String nombreGrupo, String descripcion) {
+        this.idGrupo = idGrupo;
+        this.nombreGrupo = nombreGrupo;
+        this.descripcion = descripcion;
+    }
+
+    public GrupoBean() {
+    }
+
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+    
+    
+}
