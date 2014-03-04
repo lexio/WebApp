90ea552664c871bf17fce03e754b21038804726a
dam2 <dam2@dam2-System-Product-Name>
2014-03-04 08:29:43 +0100

    GCM enviaaltagrupo y enviarpeticioncreador

---
+++ src/java/com/teide/dam/planfinder/gcm/enviarAltaGrupo.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teide.dam.planfinder.gcm;
+
+/**
+ *
+ * @author dam2
+ */
+public class enviarAltaGrupo {
+    
+}
